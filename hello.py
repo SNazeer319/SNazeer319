7d556d841c1382cf201ccbaed76d193b5c8e7c15
SNazeer319 <salmannazeer319@gmail.com>
2024-08-19 11:08:34 +0500

    Python Data Types

---
+++ hello.py
@@ -0,0 +1,125 @@
+# print("hello world")
+
+#hs = ("hello world")
+#print(hs)
+
+# legal veriable name:
+
+
+# myname = "John Doe"
+# name = "jane Doe"
+# _name = "Jane Doe"  # snake case writing style
+# nameFirst = "Jane"  # camel case writing style 
+# NameFirst = "Jane"  # pascal case writing style
+# name1st = "jane"
+# name-name = "Jane"  # hyphenated case writing
+
+# 03 variable for string and number concatenation 
+
+#name = "John Doe"
+#age = "26"
+#print("Hello,my name is"+ name+" "+age)
+
+# 04 familiar and unfamiliar operators
+
+# a = 10 
+# a = 10 + 10
+# a = a + 10
+# a + = 10
+
+# 05 math ambiguity
+
+# a = 2 + 3 * 5
+
+# 06 if condition:
+
+# x = 10
+
+# if x == 10:
+#    print("x is 10")
+# else:
+#    print("x is not 10")
+
+#x = 15
+
+#if x == 10:
+    #print("x is 10")
+#elif x == 20: 
+    #print("x is 20")
+#else:
+# print("x is neither 10 nor 20")
+
+#marks = 79
+
+#if marks >= 90:
+ #   print("Excellent! Grade A")
+#elif marks >= 80:
+ #   print("Good! Grade B")
+#else:
+ #   print("Failed! Grade F")
+
+#hp = 10
+#if hp != 10:
+ #   print("healthy")
+#else:
+ #   print("unhealthy")
+
+# 07 input
+# name = input("Salman ")
+# print("Hello,", name)
+
+
+# 08 if input condition 
+# num = 90 
+# if (num == 90):
+#    print("Grade A")
+    
+#09 if test of conditions
+
+# mark = 90
+# if mark >=90:
+#     print("Excellent! Grade A")
+# num1 = 90
+# num2 = 80
+# if (num1 > 10 or num2 > 10):
+#     print("num1 is greater than 10")
+
+#
+
+# 14 dictionaries
+
+# thisdict = {"name": "john Doe", "age":26, "city":"New York", "list": ["hello" , "world" ,"!" ]}
+# thisdict["first name"] = "Ashutosh"
+
+# print(thisdict["name"])
+
+# x = thisdict.get("name")
+# print(x)
+# thisdict.update({"last name": "hello mira"})
+# print( thisdict.values())
+# print(thisdict.keys())
+# print(thisdict) 
+
+# prnt = {
+#     "child1":{
+#         "name" : "Aslam",
+#         "age" : 24,
+#             } ,
+#         "child2":{
+#             "name" : "Ashutosh",
+#             "age" : 22,
+#         },
+#         "child3":{
+#             "name" : "Salman",
+#             "age" : 26,
+#         }
+#  }
+
+# print(prnt)
+
+# dic1 = {"name" : "john Doe", "age" : 26, "city": "new York"}
+# dic2 = dic1.copy()
+# dic1["name"] = "ashutosh"
+# print(dic1)
+# print(dic2) 
+
